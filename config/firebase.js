29033c0c626c4f239a8c39eacee1dd3daf632637
gee__een <38340742+GeeEnd@users.noreply.github.com>
2024-11-21 17:47:49 +0800

    Update firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -5,13 +5,13 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAJKEd0LjOqSO9AHK_OwqBrXD1ivfCxZaY",
-    authDomain: "crud-master-3013d.firebaseapp.com",
-    databaseURL: "https://crud-master-3013d-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "crud-master-3013d",
-    storageBucket: "crud-master-3013d.appspot.com",
-    messagingSenderId: "267781587938",
-    appId: "1:267781587938:web:629079ca405fef5b5a6fbb"
+    apiKey: "YOUR FIREBASE API KEY HERE",
+    authDomain: 'AUTH DOMAIN HERE'
+    databaseURL: 'DATABASE URL HERE'
+    projectId: 'PROJECT ID HERE'
+    storageBucket: 'STORAGE BUCKET HERE'
+    messagingSenderId: ""
+    appId: ""
 };
 
 // Initialize Firebase
